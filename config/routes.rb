b0990d90dba352c25aa6f01655ee796e9c9bd919
Cameron Westland <camwest@gmail.com>
2012-03-09 11:58:34 -0500

    Adding some course layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 WoopleTheme::Application.routes.draw do
-  resource :browse, controller: "browse"
+  resource :browse, controller: "browse" do
+    member do
+      get :course
+    end
+  end
+
   root :to => 'home#show'
 end
