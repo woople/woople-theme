7ccd5e351dbb95aa9121c9a1b9ce91e34aaeaee3
Cameron Westland <camwest@gmail.com>
2012-03-15 10:07:19 -0400

    Added header for the browse view with sorting anchors, swapped out star icon

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module WoopleTheme
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
