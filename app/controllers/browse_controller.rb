f2f6f006d333ee9833d3b8df8ced6fffdcda8836
Cameron Westland <camwest@gmail.com>
2012-03-22 15:38:39 -0400

    More tweaks

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -12,5 +12,7 @@ class BrowseController < ApplicationController
 
   def set_course_names
     @course_names = ["My Really Long Course Name That Might Have to Do With Sales", "Anatomy of Backbone.js", "Rails Testing for Zombies", "CSS Cross-Country", "CoffeeScript", "jQuery Air: Captain's Log", "Natural Language Processing", "Game Theory", "Probabilistic Graphical Models", "Design and Analysis of Algorithms I", "Human-Computer Interaction"]
+    @images = ['course.png', 'missing.png', 'coffee.png', 'leadership.png', 'training.png', 'workshops.png', 'pen.png', 'innovation.png']
+    @completed = ['completed', '']
   end
 end
