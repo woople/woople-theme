f6f0bcff7e5495394fd391f712618b2342cfe2e4
Cameron Westland <camwest@gmail.com>
2012-04-03 15:45:52 -0400

    Tested the outline presenter

--- app/presenters/outline_presenter.rb
+++ app/presenters/outline_presenter.rb
@@ -1,23 +1,43 @@
-class OutlinePresenter < SimpleDelegator
-  def initialize(outline)
-    super(outline)
-  end
+require 'delegate'
 
+class OutlinePresenter < SimpleDelegator
   def assessment
     css_class = "assessment"
-    css_class << " disabled" if !__getobj__.assessment_enabled?
+    css_class << " disabled" if assessment_disabled?
 
-    yield(css_class) if __getobj__.assessment
+    yield(css_class) if has_assessment?
   end
 
-  def render_downloads(items, view_context)
-    items = ThemePresentation.wrap_collection(items, OutlineDownloadPresenter)
-    view_context.render partial: 'woople-theme/outline_download', collection: items
+  def render_downloads(view_context)
+    wrapped_downloads = ThemePresentation.wrap_collection(downloads, OutlineDownloadPresenter)
+    view_context.render partial: 'woople-theme/outline_download', collection: wrapped_downloads
   end
 
-  def render_videos(items, view_context)
-    items = ThemePresentation.wrap_collection(items, OutlineVideoPresenter)
-    view_context.render partial: 'woople-theme/outline_video', collection: items
+  def render_videos(view_context)
+    wrapped_videos = ThemePresentation.wrap_collection(videos, OutlineVideoPresenter)
+    view_context.render partial: 'woople-theme/outline_video', collection: wrapped_videos
+  end
+
+  private
+
+  def downloads
+    outline.downloads
+  end
+
+  def videos
+    outline.videos
+  end
+
+  def assessment_disabled?
+    outline.respond_to?(:assessment_enabled?) && !outline.assessment_enabled?
+  end
+
+  def has_assessment?
+    outline.respond_to?(:assessment) && outline.assessment
+  end
+
+  def outline
+    __getobj__
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'rspec/autorun'
 require 'woople-theme'
 
 RSpec.configure do |config|
+  #config.include ActionView::TestCase::Behavior, example_group: {file_path: %r{spec/presenters}}
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
 end
