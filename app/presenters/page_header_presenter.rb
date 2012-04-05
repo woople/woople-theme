f7849c8329576460a34c050cd0eac1a3a9cffdb1
Cameron Westland <camwest@gmail.com>
2012-04-05 10:13:39 -0400

    Added name support to the page_header

--- app/presenters/page_header_presenter.rb
+++ app/presenters/page_header_presenter.rb
@@ -3,4 +3,22 @@ require_relative 'content_image'
 
 class PageHeaderPresenter < SimpleDelegator
   include ContentImage
+
+  def title
+    name || header.title
+  end
+
+  private
+
+  def name
+    if header.respond_to?(:name)
+      header.name
+    else
+      nil
+    end
+  end
+
+  def header
+    __getobj__
+  end
 end
--- spec/presenters/page_header_presenter_spec.rb
+++ spec/presenters/page_header_presenter_spec.rb
@@ -4,4 +4,22 @@ require_relative '../../spec/support/content_image_example'
 describe PageHeaderPresenter do
   let(:presenter) { PageHeaderPresenter }
   include_examples 'content_image'
+
+  describe "#title" do
+    describe "when the object has a name" do
+      subject { PageHeaderPresenter.new(stub(name: 'Name')) }
+
+      it "should have a name" do
+        subject.title.should == "Name"
+      end
+    end
+
+    describe "when the object has a title" do
+      subject { PageHeaderPresenter.new(stub(title: 'Title')) }
+
+      it "should have the title" do
+        subject.title.should == 'Title'
+      end
+    end
+  end
 end
