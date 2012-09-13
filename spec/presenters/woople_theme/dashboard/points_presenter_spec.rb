dde7b7e6ba3a762bc74cd51506d3aa2ac0001aef
Adam Doeler <adam@adamdoeler.com>
2012-09-13 16:43:03 -0400

    Fixed an infinity division by zero error

--- spec/presenters/woople_theme/dashboard/points_presenter_spec.rb
+++ spec/presenters/woople_theme/dashboard/points_presenter_spec.rb
@@ -1,16 +1,25 @@
 require 'spec_helper'
 
 describe WoopleTheme::Dashboard::PointsPresenter do
-
   describe "#percent_complete" do
+    describe "when total_points is 0" do
+      let(:data) { data = OpenStruct.new({ earned: 1, total: 0 }) }
 
-    subject do
-      data = OpenStruct.new({ earned: 1, total: 3 })
-      WoopleTheme::Dashboard::PointsPresenter.new(data).percent_complete
+      subject { WoopleTheme::Dashboard::PointsPresenter.new(data).percent_complete }
+
+      it "should return 100 when points_total is 0" do
+        subject.should eq(100)
+      end
     end
 
-    it "should return the percent points_earned / points_total" do
-      subject.should eq(33)
+    describe "when total_points is not 0" do
+      let(:data) { data = OpenStruct.new({ earned: 1, total: 3 }) }
+
+      subject { WoopleTheme::Dashboard::PointsPresenter.new(data).percent_complete }
+
+      it "should return the percent points_earned / points_total" do
+        subject.should eq(33)
+      end
     end
   end
 end
